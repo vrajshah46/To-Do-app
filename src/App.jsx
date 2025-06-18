e0264e07d8215b986bb4ea3610b0580e31ce0e47
vrajshah46 <104714518+vrajshah46@users.noreply.github.com>
2025-06-18 20:35:06 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleTask = () => {
+    setList([...list, input]);
+    setInput("");
+  };
+
+  const handleUpdate = () => {
+    const updatedList = [...list];
+    updatedList.splice(editIndex, 1, input); // Using splice for update
+    setList(updatedList);
+    setInput("");
+    setEditIndex(null);
+  };
+
+  const handleDelete = (i) => {
+    const filteredList = list.filter((_, index) => index !== i);
+    setList(filteredList);
+  };
+
+  const handleEdit = (i) => {
+    setInput(list[i]);
+    setEditIndex(i);
+  };
+
   return (
     <div className="App">
-      <h2> To-do App </h2>
+      <h2 className="heading">
+        <span className="highlight">To-do App</span>
+      </h2>
+      <div className="container">
+        <div className="input-box">
+          <input type="text" value={input} onChange={handleInput} />
+          {editIndex !== null ? (
+            <button onClick={handleUpdate}>Update</button>
+          ) : (
+            <button onClick={handleTask}>Add task</button>
+          )}
+        </div>
+        <div className="list">
+          <ul>
+            {list.map((data, i) => (
+              <li key={i}>
+                {data}
+                <div>
+                  <FaEdit
+                    onClick={() => handleEdit(i)}
+                    style={{
+                      color: "blue",
+                      marginLeft: "10px",
+                      cursor: "pointer",
+                    }}
+                  />
+                  <FaTrash
+                    onClick={() => handleDelete(i)}
+                    style={{
+                      color: "red",
+                      marginLeft: "10px",
+                      cursor: "pointer",
+                    }}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
