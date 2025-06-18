c0fd9f56c123b15c6b7a32ff7ac137eb63df3867
vrajshah46 <104714518+vrajshah46@users.noreply.github.com>
2025-06-18 18:33:52 +0530

    heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="App">
+      <h2> To-do App </h2>
+    </div>
+  );
 };
 
 export default App;
